44941c8658ffacf7a4f350c81bb29d8d282d4c80
narenSb1837 <89464601+narenSb1837@users.noreply.github.com>
2023-12-28 00:16:33 +0530

    vect_embed.py

--- vect_embed.py
+++ vect_embed.py
@@ -1,7 +1,7 @@
 
 import os
 os.environ["COHERE_API_KEY"]='P7b3enduxOgEYHdhY4X0xBu7j7Q125sAZ3MMdSWy'
-os.environ['PINECONE_API_KEY']='b31ccf3f-4c73-4851-abe6-e0653439dab5'#c85ea1ee-1d0f-4c78-9285-1dd05c5194aa'
+os.environ['PINECONE_API_KEY']='809adeaf-d6ce-423a-940e-041ce9b8614b'#c85ea1ee-1d0f-4c78-9285-1dd05c5194aa'
 os.environ['PINECONE_ENV']='gcp-starter'
 import cohere
 import langchain
